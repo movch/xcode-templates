505a8ac10d0cf0045fb864d106540aba5e444dc5
Никита Красавин <6nike6@bk.ru>
2019-09-04 12:55:04 +0300

    add app center configuration

--- HandsApp iOS App Template.xctemplate/AppDelegate.swift
+++ HandsApp iOS App Template.xctemplate/AppDelegate.swift
@@ -6,11 +6,22 @@
 //  ___COPYRIGHT___
 //
 
+import AppCenter
+import AppCenterAnalytics
+import AppCenterDistribute
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
+    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        startAppCenter()
+        return true
+    }
+
+    private func startAppCenter() {
+        let services: [AnyClass] = [MSAnalytics.self, MSDistribute.self]
+        MSAppCenter.start(ApiKeys.appCenter, withServices: services)
+    }
 }
