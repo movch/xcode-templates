98794713901784399732d6401849568505e6980d
Никита Красавин <6nike6@bk.ru>
2019-12-23 13:52:39 +0300

    - update AppDelegate with setupWindow() - fix bug: add swift format

--- HandsApp iOS App Template.xctemplate/AppDelegate.swift
+++ HandsApp iOS App Template.xctemplate/AppDelegate.swift
@@ -1,9 +1,9 @@
 import AppCenter
 import AppCenterAnalytics
-import AppCenterDistribute
 import AppCenterCrashes
-import UIKit
+import AppCenterDistribute
 import HandsAppUI
+import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, HandsAppDelegate {
@@ -15,11 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate, HandsAppDelegate {
 
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         startAppCenter()
+        setupWindow()
         return true
     }
 
     private func startAppCenter() {
         let services: [AnyClass] = [MSAnalytics.self, MSDistribute.self, MSCrashes.self]
         MSAppCenter.start(ApiKeys.appCenter, withServices: services)
+    }
+
+    private func setupWindow() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
     }
 }
